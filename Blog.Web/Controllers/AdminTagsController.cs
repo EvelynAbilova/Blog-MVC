bb1c6d599156fe87c413f6c4276674a2aa6ae700
EvelynAbilova <evelyn.abilova@gmail.com>
2024-02-10 01:42:42 +0300

    feat: basic layout and CRUD operations

---
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,106 @@
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly ITagRepository tagRepository;
+
+        public AdminTagsController(ITagRepository tagRepository)
+        {
+            this.tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {       
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public async Task<IActionResult> Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            await tagRepository.AddAsync(tag);
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public async Task<IActionResult> List() 
+        {
+            var tags = await tagRepository.GetAllAsync();
+
+            return View(tags);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id) 
+        {
+            var tag = await tagRepository.GetAsync(id);
+
+            if (tag != null) 
+            {
+                var editTagRequest = new EditTagRequest 
+                { 
+                    Id = tag.Id,
+                    Name = tag.Name, 
+                    DisplayName = tag.DisplayName
+                };
+                return View(editTagRequest);
+            }
+
+            return View(); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+
+            var updatedTag = await tagRepository.UpdateAsync(tag);
+
+            if (updatedTag != null)
+            {
+                //Show success notificiation
+            }
+            else
+            {
+                //Show error notificiation
+            }
+
+            return RedirectToAction("Edit", new { id = editTagRequest.Id }); 
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(EditTagRequest editTagRequest)
+        {
+            var deletedTag = await tagRepository.DeleteAsync(editTagRequest.Id);
+
+            if (deletedTag != null)
+            {
+                //Show success notificiation
+                return RedirectToAction("List");
+            }
+
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+    }
+}
---
+++ Blog.Web/Data/BlogDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BlogPost>()
+                .Property(bp => bp.PublishedDate)
+                .HasConversion(
+                    v => v.ToUniversalTime(),
+                    v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+        }
+
+
+}
+}
