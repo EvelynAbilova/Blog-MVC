a57e89ccb2c4566b36c1c787409af6829ab80db2
EvelynAbilova <evelyn.abilova@gmail.com>
2024-02-11 02:55:48 +0300

    feat: display posts and tags on homepage

---
+++ Blog.Web/Models/ViewModels/HomeViewModelcs.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Web.Models.Domain;
+
+namespace Blog.Web.Models.ViewModels
+{
+    public class HomeViewModelcs
+    {
+        public IEnumerable<BlogPost> BlogPosts { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+    }
+}
